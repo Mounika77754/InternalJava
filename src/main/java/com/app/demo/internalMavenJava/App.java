a2117b1f5ed8fa08ee2b09786bde94488a3b9afb
Mounika77754 <mouni77754@gmail.com>
2024-12-29 16:04:59 +0530

    done

---
+++ src/main/java/com/app/demo/internalMavenJava/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.internalMavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
